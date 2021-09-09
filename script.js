86131b03058da6d630876f033d9e8d68f8f7c74a
LINAULIA <linaulia2001@gmail.com>
2021-09-09 20:50:27 +0700

    Menambahkan fungsi await

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 const nilai =["70", "75", "80", "85", "90", "95", "100"];
+const fungsiAwait= async ()=> {
+await showNilai ();
+await tambahan();
+}
 
-const tampilkan=()=> {
+
+const showNilai=()=> {
   var valueNilai = document.getElementById("nilai").value;
   var valueNama = document.getElementById("nama").value;
-  (nilai.indexOf(valueNilai) >-1)?console.log(valueNama+" nilai kamu "+valueNilai+" : Lulus"):console.log(valueNama+" nilai kamu "+valueNilai+" : Maaf kamu belum lulus");
+  (nilai.indexOf(valueNilai) >-1)?console.log(valueNama+" nilai kamu "+valueNilai+" : Selamat Kamu Lulus"):console.log(valueNama+" nilai kamu "+valueNilai+" : Maaf kamu belum lulus");
+}
+
+
+const tambahan=()=> {
+  var valueNilai = document.getElementById("nilai").value;
+  (nilai.indexOf(valueNilai) >-1)?console.log("Perhatankan!"):console.log("Yuk bisa yuk!");
 }
